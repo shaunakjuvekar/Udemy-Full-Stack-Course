72ae07ae1fe19c54855ac3d4529857dab6d8b191
Gamershaun <ashu.gupta3@vodafone.com>
2020-03-23 18:30:01 +0530

    updated files

--- Udemy Full Stack Course/Dicee Challenge/index.js
+++ Udemy Full Stack Course/Dicee Challenge/index.js
@@ -2,14 +2,12 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1
 console.log(randomNumber1)
 var image1 = document.querySelector(".img1").src="images/dice"+randomNumber1+".png";
-console.log(image1) 
-var val1=dict[image1];
+console.log(image1)
 
 var randomNumber2 = Math.floor(Math.random()*6)+1
 console.log(randomNumber2)
 var image2 = document.querySelector(".img2").src="images/dice"+randomNumber2+".png";
 console.log(image2) 
-var val2=dict[image2];
 
 if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="Player 1 Wins!!"
