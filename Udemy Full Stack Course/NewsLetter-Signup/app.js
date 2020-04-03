989e477e4e4ffadbbe2d0f4386bd6745b6a68810
Shaunak <gamershaunj@gmail.com>
2020-04-03 15:41:41 +0530

    added code

--- Udemy Full Stack Course/NewsLetter-Signup/app.js
+++ Udemy Full Stack Course/NewsLetter-Signup/app.js
@@ -4,8 +4,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/",function(req,res){
+    console.log(req.body.Firstname);
+    res.send(req.body.Firstname);
+})
 
 
 
