bbbd004260e05bb9219a761bca77d5f5088bce60
Shaunak <gamershaunj@gmail.com>
2020-04-03 13:32:53 +0530

    restructuring plus new files

---
+++ Udemy Full Stack Course/NewsLetter-Signup/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+})
--- Udemy Full Stack Course/WeatherProject/app.js
+++ Udemy Full Stack Course/WeatherProject/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get("/",function(req,res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=1f661e1dce175da0c533091e936d8ee8&units=metric"
+    res.sendFile(__dirname+"/index.html")
+
+    
+})
+
+app.post("/",function(req,res){
+
+
+    const cityName = req.body.cityName;
+    const apiKey = "1f661e1dce175da0c533091e936d8ee8"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+ apiKey+"&units=metric"
    
     https.get(url,function(response){
-        //console.log(response);
 
         response.on("data",function(data){
             const weatherData = JSON.parse(data);
@@ -18,7 +31,7 @@ app.get("/",function(req,res){
             const icon = weatherData.weather[0].icon;
             
            
-            res.write("<h1>The temperature in Pune is " + temp + " Degree Celsius</h1>");
+            res.write("<h1>The temperature in " + cityName + " is " + temp + " Degree Celsius</h1>");
             res.write("<p>The weather description is currently: " + description +"</p>");
             const imgurl = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
             res.write("<img src="+imgurl+">");     
@@ -28,7 +41,9 @@ app.get("/",function(req,res){
         })
 
     })
+
 })
+
 
 
 app.listen(3000,function(){
