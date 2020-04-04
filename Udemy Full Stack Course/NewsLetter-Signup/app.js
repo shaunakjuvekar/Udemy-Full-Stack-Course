553dcf7ca871645f5dabda52e784080397d07d5f
Shaunak <gamershaunj@gmail.com>
2020-04-04 12:52:40 +0530

    updated app.js

--- Udemy Full Stack Course/NewsLetter-Signup/app.js
+++ Udemy Full Stack Course/NewsLetter-Signup/app.js
@@ -6,29 +6,60 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
 })
 
-app.post("/",function(req,res){
-    
-    var firstName = req.body.Firstname;
-    var lastName  = req.body.Lastname;
-    var email     = req.body.Email;
-    
-    console.log(firstName, lastName, email);
+app.post("/", function (req, res) {
 
+    const firstName = req.body.Firstname;
+    const lastName = req.body.Lastname;
+    const email = req.body.Email;
+
+    const url = "https://us19.api.mailchimp.com/3.0/lists/2301d31517";
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    }
+
+    const jsonData =  JSON.stringify(data)  
+
+    const options = {
+        method: "post",
+        auth: "shaun1:47d815ec9fc7034781a2e08f10625b3e-us19"
+    }
+
+    const request = https.request(url,options,function(response){
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 })
 
 
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 })
 
 
-// API KEY
+// API KEY  
 // 47d815ec9fc7034781a2e08f10625b3e-us19
+
+// List ID
+// 2301d31517
